9f1a3ad59a099cfafebb659da29c6090e8afdc1f
Radosław Biesek <radoslaw.biesek@gmail.com>
2019-02-10 18:28:25 +0100

    Create different routes

--- server.js
+++ server.js
@@ -3,9 +3,32 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('Hello, world');
+    console.log('Otrzymalem żądanie GET do strony głównej');
+    res.send('Hello GET');
 });
 
-var server = app.listen(3000, function() {
-    console.log('App is listening on localhost:3000');
-})
+app.post('/', function(req, res) {
+    console.log('Otrzymałem żądanie POST do strony głównej');
+    res.send('Hello POST');
+});
+
+app.delete('/del_user', function(req, res) {
+    console.log('Otrzymałem żądanie DELETE do strony /del_user');
+    res.send('Hello DELETE');
+});
+
+app.get('/list_user', function(req, res) {
+    console.log('Otzymałem zapytanie GET do strony /list_user');
+    res.send('Strona z listą użytkowników');
+});
+
+app.get('ab*cd', function(req, res) {
+    console.log('Otrzymałem żądanie GET do strony /ab*cd');
+    res.send('Wzór pasuje');
+});
+
+app.use(function(req, res, next) {
+    res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego czego żądasz!');
+});
+
+var server = app.listen(3000);
